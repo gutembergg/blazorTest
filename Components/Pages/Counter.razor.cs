a63579227ec3e772be2968640c9a90435e38d035
gutembergg <gmascarenhas3001@gmail.com>
2024-01-04 00:28:00 +0100

    first commit

---
+++ Components/Pages/Counter.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorAppTest.Components.Pages
+{
+    public class CounterBase : ComponentBase
+    {
+        protected int currentCount = 0;
+
+        [Parameter]
+        public int Value { get => currentCount; set => currentCount = value; }
+
+        protected void IncrementCount()
+        {
+            Console.WriteLine("IncrementCount is called");
+            currentCount++;
+        }
+    }
+}
---
+++ Components/Pages/MagicNumber.razor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorAppTest.Components.Pages
+{
+    public class MagicNumberBase : ComponentBase
+    {
+        protected const int NumberLifeMax = 5;
+        protected const int NumberMax = 20;
+
+        protected int value;
+        protected int NumberMagic;
+        protected int NumberRemainigLife;
+        protected bool? GameWon;
+
+
+        protected override void OnInitialized()
+        {
+            var random = new Random();
+            NumberMagic = random.Next(NumberMax);
+
+            NumberRemainigLife = NumberLifeMax;
+
+            base.OnInitialized();
+        }
+
+        protected void TestNumber()
+        {
+            Console.WriteLine("TestNumber is called");
+            if(value == NumberMagic)
+            {
+                GameWon = true;
+            } else
+            {
+                NumberRemainigLife--;
+
+                if(NumberRemainigLife == 0)
+                {
+                    GameWon = false;
+                }
+            }
+        }
+    }
+}
